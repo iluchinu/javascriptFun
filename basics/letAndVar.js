c3c7b30ed6afe0252418ee27a540b5cb81ca1adf
Ravishankar <k.ravishankar111@gmail.com>
2023-11-03 11:46:28 +0000

    updated example

--- basics/letAndVar.js
+++ basics/letAndVar.js
@@ -12,10 +12,42 @@ function varScope(){
 function letScope(){
     let b=5;
     if(true){
-        let b=2;
+       let b=2;
         console.log(b);
     }
     console.log(b);
 }
 
-letScope();
+//letScope();
+
+let x=10;
+function letGlobalScope(){
+    let x=100;
+    console.log(x);
+}
+
+// letGlobalScope();
+// console.log(x);
+
+var y=10;
+function varGlobalScope(){
+    var y=100;
+    console.log(y);
+}
+
+// varGlobalScope();
+// console.log(y);
+
+var abc=500;
+{
+    var abc=100;
+    console.log(abc);
+}
+console.log(abc);
+
+let xyz=500;
+{
+    let xyz=100;
+    console.log(xyz);
+}
+console.log(xyz);
