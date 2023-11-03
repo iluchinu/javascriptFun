9fcd2851bc18db1495d618a33022c9be919be6a0
Ravishankar <k.ravishankar111@gmail.com>
2023-11-03 09:31:11 +0000

    console table

---
+++ basics/variables.js
@@ -0,0 +1,7 @@
+const accountId= 101;
+let accountEmail= "ravishankar.k@gmail.com";
+var accountPassword= "Test@12345";
+accountCity= "Bengaluru";
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
