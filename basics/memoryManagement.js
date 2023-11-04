c3a656a9367a8798cf3eaa51bf19ff467bccc6f7
Ravishankar <k.ravishankar111@gmail.com>
2023-11-04 08:24:11 +0000

    memory management

---
+++ basics/memoryManagement.js
@@ -0,0 +1,18 @@
+let myYoutubename = "thisJourneyZFun";
+let anotherName = myYoutubename;
+anotherName= "iPhone15proMax";
+
+console.log(myYoutubename);
+console.log(anotherName);
+
+let userOne = {
+    email: "shankar.ravi014@gmail.com",
+    upi: "user@ybl"
+
+};
+
+let userTwo=userOne;
+userTwo.email = "k.ravishankar111@gmail.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
