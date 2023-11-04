5bf100034d0a0522b1786d3034ea264df18cbb6f
Ravishankar <k.ravishankar111@gmail.com>
2023-11-04 00:53:20 +0000

    conversion

---
+++ basics/conversionOperation.js
@@ -0,0 +1,20 @@
+//const score = "33"
+//const score = "33abc";
+//const score = null;
+//const score = undefined;
+const score = true;
+
+const validNumber = Number(score);
+// console.log(typeof validNumber);
+// console.log(validNumber);
+
+let isLoggedIn = -3;
+// let isLoggedIn= "";
+
+let booleanIsLoggedIn= Boolean(isLoggedIn);
+// console.log(booleanIsLoggedIn);
+
+const tempNumber = 33;
+const stringTempNumber = String(tempNumber);
+console.log(stringTempNumber)
+console.log(typeof stringTempNumber);
