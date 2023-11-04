6dc7c00a66bee7ce70e6d10845984304b1340a12
Ravishankar <k.ravishankar111@gmail.com>
2023-11-04 01:18:09 +0000

    updated

--- basics/conversionOperation.js
+++ basics/conversionOperation.js
@@ -19,8 +19,13 @@ const stringTempNumber = String(tempNumber);
 // console.log(stringTempNumber)
 // console.log(typeof stringTempNumber);
 
-console.log("1"+2);
-console.log(1+"2");
-console.log("1"+2+2);
-console.log(1+2+"2");
-console.log(+true);
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// console.log(+true);
+
+let counter= 100;
+// counter++;
+++counter;
+console.log(counter);
