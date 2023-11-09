28d14c6d19fbfcad5141355cb727b77a99692c6c
Ravishankar <k.ravishankar111@gmail.com>
2023-11-09 01:48:19 +0000

    number and math

---
+++ basics/math.js
@@ -0,0 +1,17 @@
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.357));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4, 2, 89, -1));
+// console.log(Math.max(-89, -1, 0, -34));
+
+// console.log(Math.random());
+// console.log(Math.random()*10);
+// console.log(Math.floor(Math.random()*10)+1);
+
+const min =10;
+const max= 20;
+
+// console.log(Math.floor(Math.random() * ((max-min)+1)));
+console.log(Math.floor(Math.random() * (max-min+1))+min);
--- basics/string.js
+++ basics/string.js
@@ -1,23 +1,26 @@
 const firstName = "Mariswamy";
 const fname = new String("karthick-on");
 
-console.log(fname[0]);
-console.log(firstName[0]);
+console.log(firstName);
+console.log(fname);
 
-console.log(firstName.toUpperCase());
-console.log(fname.toUpperCase());
-console.log(fname.charAt(2));
-console.log(fname.indexOf('h'));
+// console.log(fname[0]);
+// console.log(firstName[0]);
 
-const newString = fname.substring(0, 4);
-console.log(newString);
+// console.log(firstName.toUpperCase());
+// console.log(fname.toUpperCase());
+// console.log(fname.charAt(2));
+// console.log(fname.indexOf('h'));
 
-const anotherString = fname.slice(-2, 4);
-console.log(anotherString);
+// const newString = fname.substring(0, 4);
+// console.log(newString);
 
-console.log(fname.includes("x"));
+// const anotherString = fname.slice(-2, 4);
+// console.log(anotherString);
 
-console.log(fname.split("-"));
+// console.log(fname.includes("x"));
 
-console.log(fname.concat("Ravi"));
-console.log(fname.endsWith("n"));
+// console.log(fname.split("-"));
+
+// console.log(fname.concat("Ravi"));
+// console.log(fname.endsWith("n"));
