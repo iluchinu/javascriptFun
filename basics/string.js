b082a8f354e6871cd327f0b0d91d976d7fa493da
Ravishankar <k.ravishankar111@gmail.com>
2023-11-09 00:30:15 +0000

    string operation

---
+++ basics/string.js
@@ -0,0 +1,23 @@
+const firstName = "Mariswamy";
+const fname = new String("karthick-on");
+
+console.log(fname[0]);
+console.log(firstName[0]);
+
+console.log(firstName.toUpperCase());
+console.log(fname.toUpperCase());
+console.log(fname.charAt(2));
+console.log(fname.indexOf('h'));
+
+const newString = fname.substring(0, 4);
+console.log(newString);
+
+const anotherString = fname.slice(-2, 4);
+console.log(anotherString);
+
+console.log(fname.includes("x"));
+
+console.log(fname.split("-"));
+
+console.log(fname.concat("Ravi"));
+console.log(fname.endsWith("n"));
