77c6514b21af618d546aa8797e43c52b92f825db
Ravishankar <k.ravishankar111@gmail.com>
2025-09-26 11:06:10 +0000

    letAndVar

---
+++ basics2025/letVar.js
@@ -0,0 +1,18 @@
+let x=5;
+var y=10;
+
+if(y>=5){
+    let x= 15;
+    x=x+10;
+    console.log(x);
+}
+
+console.log(x);
+
+if(x>=5){
+    var y=100;
+    y=y+100;
+    console.log(y);
+}
+
+console.log(y);
